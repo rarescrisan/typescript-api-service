5248801a87d4e4d17efdc86b4583039a4c067a51
Jake Deichert <git@jakedeichert.com>
2019-11-17 13:47:21 -0500

    Adjust formatting for json and yaml

--- .prettierrc.js
+++ .prettierrc.js
@@ -2,4 +2,12 @@ module.exports = {
     tabWidth: 4,
     singleQuote: true,
     trailingComma: 'es5',
+    overrides: [
+        {
+            files: ['*.json', '*.yml'],
+            options: {
+                tabWidth: 2,
+            },
+        },
+    ],
 };
