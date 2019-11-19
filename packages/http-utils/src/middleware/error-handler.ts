387e2e9b535ade6e09a8f302c8e29fc461a4db24
Jake Deichert <git@jakedeichert.com>
2019-11-19 12:05:21 -0500

    Adjust error handler

--- packages/http-utils/src/middleware/error-handler.ts
+++ packages/http-utils/src/middleware/error-handler.ts
@@ -1,35 +1,52 @@
 import { Next, Middleware } from '../middleware';
 import { Context } from '../index';
 
-export default (shouldLogErrors: boolean) => {
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+type Err = any;
+
+export default (shouldLogWarnings: boolean) => {
     return async (ctx: Context, next: Next): Middleware => {
         try {
             await next();
         } catch (err) {
-            // Format non-500 errors
-            if (err.isBoom && err.output.statusCode < 500) {
-                ctx.status = err.output.statusCode;
-                ctx.body = {
-                    err: true,
-                    msg: err.message,
-                };
-                if (!shouldLogErrors) return; // Possibly skip logging of errors
-                ctx.logger.warn(err, getErrorMeta(err));
-                ctx.app.emit('error', err, ctx);
-            } else {
-                ctx.status = 500;
-                ctx.body = {
-                    err: true,
-                    msg: 'Internal Service Error',
-                };
-                ctx.logger.error(err.stack, getErrorMeta(err));
-                ctx.app.emit('error', err, ctx);
-            }
+            createErrorResponse(ctx, err);
+            logError(ctx, ctx.status, err, shouldLogWarnings);
         }
     };
 };
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-function getErrorMeta(err: any): KeyValueMap {
-    return (err && err.data) || {};
+function createErrorResponse(ctx: Context, err: Err): void {
+    const statusCode = err.isBoom ? err.output.statusCode : 500;
+
+    ctx.status = statusCode;
+    ctx.body = {
+        err: true,
+        msg: err.message,
+    };
+
+    if (statusCode >= 500) {
+        // Don't let the client see any internal error details
+        ctx.body.msg = 'Internal Service Error';
+    }
+}
+
+function logError(
+    ctx: Context,
+    statusCode: number,
+    err: Err,
+    shouldLogWarnings: boolean
+): void {
+    const errMeta = err.data || {};
+
+    // Possibly log a warning
+    if (statusCode < 500) {
+        if (!shouldLogWarnings) return;
+        ctx.logger.warn(err, errMeta);
+        ctx.app.emit('error', err, ctx);
+        return;
+    }
+
+    // Log an error
+    ctx.logger.error(err.stack, errMeta);
+    ctx.app.emit('error', err, ctx);
 }
