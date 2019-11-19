100cd7643e7d13bebdf236c1dac5ba53a3c31343
Jake Deichert <git@jakedeichert.com>
2019-11-19 12:32:24 -0500

    Adjust logger

--- packages/logger/src/formatter.ts
+++ packages/logger/src/formatter.ts
@@ -1,16 +1,16 @@
 import { format } from 'winston';
 
 const formatter = format.printf(({ level, message, timestamp, reqId, ctx }) => {
-    if (!ctx) return `${timestamp} [${level}] >> ${message}`;
+    if (!ctx) return `${timestamp} [${level}] ${message}`;
 
     const reqUrl = ctx.request.url;
     const respCode = ctx.status;
 
     if (respCode && ctx.body) {
-        return `${timestamp} [${level}] (id ${reqId} :: ${reqUrl} :: ${respCode}) >> ${message}`;
+        return `${timestamp} [${level}] [id ${reqId} :: ${reqUrl} :: ${respCode}] ${message}`;
     }
 
-    return `${timestamp} [${level}] (id ${reqId} :: ${reqUrl}) >> ${message}`;
+    return `${timestamp} [${level}] [id ${reqId} :: ${reqUrl}] ${message}`;
 });
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
